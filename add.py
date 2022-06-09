a74bbb991e3a05749ce619b7c636e3227fa5036f
Esaimozhi3 <esaimozhi3@gmail.com>
2022-06-09 14:33:42 +0530

    addition for three numbers

--- add.py
+++ add.py
@@ -1,7 +1,8 @@
-def add(n1,n2):
-	c = n1 + n2
+def add(n1,n2,n3):
+	c = n1 + n2 +n3
 	return c
-n1 = int(input("enter the first number"))
-n2 = int(input("enter the second number"))
-print(add(n1,n2))
+n1 = int(input("enter the first number : "))
+n2 = int(input("enter the second number : "))
+n3 = int(input("enter the second number : "))
+print("the addition is : 3",add(n1,n2,n3))
 
